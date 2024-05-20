ec354b061da6d33f68dd641cb0d469040224234d
toughluck007 <camenator@msn.com>
2024-05-20 13:22:19 -0400

    Diamond Only

--- sketch.js
+++ sketch.js
@@ -1,121 +1,48 @@
-let letters = [];
-let assembled = false;
-let rotationSpeed = 0.01;
 let angle = 0;
-let font;
 let backgroundColor = '#40403f';
-let diamondColor;
-let textSizeFactor = 0.1; // Factor to adjust text size based on canvas height
-
-function preload() {
-  font = loadFont('BebasNeue-Regular.ttf'); // Ensure the font file is in the same directory as your sketch
-}
+let diamondColors;
+let colorIndex = 0;
+let colorLerp = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  textFont(font); // Apply the font
-  diamondColor = [color(128, 0, 128), color(255, 20, 147), color(0, 0, 255)]; // Purple, pink, blue
-  
-  setupLetters();
-}
-
-function setupLetters() {
-  letters = [];
-  let txtSize = height * textSizeFactor; // Dynamic text size based on canvas height
-  textSize(txtSize);
-  
-  // Calculate the total width of the word
-  let bounds = font.textBounds("SYNTHOGRAF", 0, 0, txtSize);
-  let totalWidth = bounds.w;
-  let targetX = -totalWidth / 2;
-  
-  // Initialize letters with alpha set to 0
-  for (let char of "SYNTHOGRAF") {
-    let points = font.textToPoints(char, 0, 0, txtSize, {
-      sampleFactor: 0.2,
-      simplifyThreshold: 0
-    });
-
-    let letter = {
-      char: char,
-      points: points,
-      x: targetX + bounds.w / "SYNTHOGRAF".length / 2,
-      y: 0,
-      z: 0,
-      targetX: targetX + bounds.w / "SYNTHOGRAF".length / 2,
-      targetY: 0,
-      targetZ: 0,
-      alpha: 0 // Starting alpha value
-    };
-    targetX += bounds.w / "SYNTHOGRAF".length;
-    letters.push(letter);
-  }
-
-  assembled = false; // Reset assembled flag
+  diamondColors = [
+    [color(0, 0, 255), color(128, 0, 128), color(255, 20, 147)], // Blue, purple, pink
+    [color(128, 0, 128), color(255, 20, 147), color(0, 0, 255)], // Purple, pink, blue
+    [color(255, 20, 147), color(0, 0, 255), color(128, 0, 128)]  // Pink, blue, purple
+  ];
 }
 
 function draw() {
   background(backgroundColor);
-  if (!assembled) {
-    fadeInLetters();
-  } else {
-    rotateWord();
-  }
-
-  for (let letter of letters) {
-    push();
-    translate(letter.x, letter.y, letter.z);
-    drawFlatLetter(letter.points, letter.alpha);
-    pop();
-  }
-
+  
+  // Update the gradient colors
+  updateGradientColors();
+  
   // Draw the spinning diamond
   push();
-  translate(0, -height / 4, 0); // Position the diamond above the word
+  translate(0, 0, 0); // Center the diamond
   rotateY(angle);
   drawDiamond();
   pop();
+
+  // Update the rotation angle
+  angle += 0.01;
 }
 
-function fadeInLetters() {
-  let allFadedIn = true;
-  for (let letter of letters) {
-    letter.alpha = lerp(letter.alpha, 255, 0.05);
-    
-    if (letter.alpha < 254) { // Slightly less than 255 to avoid rounding issues
-      allFadedIn = false;
-    }
+function updateGradientColors() {
+  // Update the lerp factor
+  colorLerp += 0.01;
+  if (colorLerp > 1) {
+    colorLerp = 0;
+    colorIndex = (colorIndex + 1) % diamondColors.length;
   }
-  if (allFadedIn) {
-    assembled = true;
-  }
-}
-
-function rotateWord() {
-  angle += rotationSpeed;
-}
-
-function drawFlatLetter(points, alpha) {
-  noStroke();
-
-  // Simulate chrome effect with gradient
-  let c1 = color(200, 200, 200, alpha); // Light gray with alpha
-  let c2 = color(50, 50, 50, alpha);    // Dark gray with alpha
-
-  // Draw the front face
-  beginShape();
-  for (let i = 0; i < points.length; i++) {
-    let inter = lerpColor(c1, c2, points[i].y / (height * textSizeFactor));
-    fill(inter);
-    vertex(points[i].x, points[i].y, 0);
-  }
-  endShape(CLOSE);
 }
 
 function drawDiamond() {
-  let c1 = diamondColor[0];
-  let c2 = diamondColor[1];
-  let c3 = diamondColor[2];
+  let c1 = lerpColor(diamondColors[colorIndex][0], diamondColors[(colorIndex + 1) % diamondColors.length][0], colorLerp);
+  let c2 = lerpColor(diamondColors[colorIndex][1], diamondColors[(colorIndex + 1) % diamondColors.length][1], colorLerp);
+  let c3 = lerpColor(diamondColors[colorIndex][2], diamondColors[(colorIndex + 1) % diamondColors.length][2], colorLerp);
   let gradient = [c1, c2, c3];
   let sizeFactor = height / 10; // Dynamic size based on canvas height
 
@@ -155,5 +82,4 @@ function drawDiamond() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  setupLetters(); // Recalculate letter positions and sizes on resize
 }
